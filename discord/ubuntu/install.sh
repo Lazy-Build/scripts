b43c61a0c34343939c338b26b3b89cb7207f2042
Austin Kregel <austin@kregel.co>
2021-11-18 23:31:09 -0500

    Readd packages

--- discord/ubuntu/install.sh
+++ discord/ubuntu/install.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-apt install -y libasound2 libatomic1 libgconf-2-4 libnotify4 libc++1
+apt install -y libasound2 libatomic1 libgconf-2-4 libnotify4 libnspr4 libnss3 libxss1 libc++1 libappindicator1
 
 curl -L "https://discord.com/api/download?platform=linux&format=deb" --output discord.deb
 
