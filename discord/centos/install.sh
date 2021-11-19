c5e2c019b1a7af8094cd379a837c9ae65e652e89
Austin Kregel <austin@kregel.co>
2021-11-19 00:34:40 -0500

    Add discord (#2)

---
+++ discord/centos/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+curl -L "https://discord.com/api/download?platform=linux&format=tar.gz" --output discord.tar.gz
+
+tar -xvf discord.tar.gz
+
+mv Discord /usr/share/discord
+
+mv /usr/share/discord/discord.desktop /usr/share/applications/
---
+++ discord/ubuntu/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+apt install -y libasound2 libatomic1 libgconf-2-4 libnotify4 libnspr4 libnss3 libxss1 libc++1 libappindicator1
+
+curl -L "https://discord.com/api/download?platform=linux&format=deb" --output discord.deb
+
+dpkg -i discord.deb
