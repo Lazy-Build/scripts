1b1e09eb9f8a8cfef32f5793b3c5ad2eeab7c489
Austin Kregel <austin@kregel.co>
2021-11-18 23:35:00 -0500

    Check for package on debian

--- discord/debian/install.sh
+++ discord/debian/install.sh
@@ -3,6 +3,8 @@ set -x
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
+apt-cache search libappindicator1
+
 apt install -y libasound2 libatomic1 libgconf-2-4 libnotify4 libnspr4 libnss3 libxss1 libc++1 libappindicator1
 
 curl -L "https://discord.com/api/download?platform=linux&format=deb" --output discord.deb
