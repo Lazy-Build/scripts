87907ddeff617a9586861bc3f7313b078e10b714
Austin Kregel <austin@kregel.co>
2021-11-18 22:49:52 -0500

    Add discord

---
+++ discord/centos/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+yum install -y libc++ libnotify
+
+curl -L https://discord.com/api/download?platform=linux&format=tar.gz --output discord.tar.gz
+
+tar -xvf discord.tar.gz
+
+mv Discord /usr/share/discord
+
+mv /usr/share/discord/Discord.desktop /usr/share/applications/
---
+++ discord/ubuntu/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+yum install -y libc++ libnotify
+
+curl -L https://discord.com/api/download?platform=linux&format=deb --output discord.deb
+
+dpkg -i discord.deb
