26ff0f0e250e35cbf4025a554ca981a5c560cf2f
Austin Kregel <austin@kregel.co>
2021-11-18 23:16:15 -0500

    Dope! Quote the thing!

--- discord/ubuntu/install.sh
+++ discord/ubuntu/install.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 
-curl -L https://discord.com/api/download?platform=linux&format=deb --output discord.deb
+curl -L "https://discord.com/api/download?platform=linux&format=deb" --output discord.deb
 
 dpkg -i discord.deb
