ce9f6b6b6fb49fe821adfbe8a6d0b3636d94b94e
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2023-11-11 19:41:36 -0500

    Update and rename 18 to 18.sh

---
+++ nodejs/ubuntu/18.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+apt update
+apt-get install nodejs -y
+
+npm i -g yarn 
+echo ""
+echo "##########################################################################################################"
+echo "                  Installation finished. You may now use Node, NPM, and Yarn"
+echo "##########################################################################################################"
+echo ""
